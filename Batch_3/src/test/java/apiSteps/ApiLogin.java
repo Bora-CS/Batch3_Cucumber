f1179c2824a93df4d686cc5d25b3444d104a87e9
nurzatEjat <nurzatejat@live.com>
2019-10-31 20:49:19 -0400

    Nur 10/31

--- Batch_3/src/test/java/apiSteps/ApiLogin.java
+++ Batch_3/src/test/java/apiSteps/ApiLogin.java
@@ -28,15 +28,15 @@ public class ApiLogin {
 
 	@When("user login with {string} and {string}")
 	public void user_login_with_and(String emailId, String password) {
-		//add header
+		// add header
 		request.headers("Content-type", "Application/json");
-		//add body,  we add body as JSON type
-		//{ "email": "john.doe@gmail.com", "password": “123456” }
+		// add body, we add body as JSON type
+		// { "email": "john.doe@gmail.com", "password": “123456” }
 		JSONObject body = new JSONObject();
 		body.put("email", emailId);
 		body.put("password", password);
 		request.body(body.toJSONString());
-		resp =  request.post("/api/users/login");
+		resp = request.post("/api/users/login");
 	}
 
 	@Then("veirfy status code {int}")
@@ -44,12 +44,38 @@ public class ApiLogin {
 		// get status code from the api call
 		// and compare the status code with given code ->status
 		int actualStatus = resp.getStatusCode();
-		String errorMessage = "Status code mismatch, expecting status is: "+status+ 
-				"  Actual Status is: "+actualStatus ;
-		
-		
-		Assert.assertEquals(errorMessage,status, actualStatus);
-		
+		String errorMessage = "Status code mismatch, expecting status is: " + status + "  Actual Status is: "
+				+ actualStatus;
+		Assert.assertEquals(errorMessage, status, actualStatus);
 		System.out.println(resp.getBody().asString());
 	}
+
+	@When("register user by {string} {string} {string} {string}")
+	public void register_user_by(String userName, String emailAddress, String password, String password2) {
+
+		request.headers("Content-type", "Application/json");
+
+		JSONObject obj = new JSONObject();
+		obj.put("name", userName);
+		obj.put("email", emailAddress);
+		obj.put("password", password);
+		obj.put("password2", password2);
+
+		request.body(obj.toJSONString());
+		resp = request.post("/api/users/register");
+	}
+
+	@Then("verify name {string} and email {string} is correct")
+	public void verify_name_and_email_is_correct(String userName, String emailAddress) {
+
+		String nameFromBody = resp.getBody().jsonPath().getString("name");
+		String emailFromBody = resp.getBody().jsonPath().getString("email");
+		
+		Assert.assertTrue("user name is missmatch: "+nameFromBody,
+				nameFromBody.equals(userName));
+		
+		Assert.assertTrue("email address is missmatch :"+emailFromBody, emailFromBody.equals(emailAddress));
+
+	}
+
 }
--- Batch_3/src/test/resources/apiLogin.feature
+++ Batch_3/src/test/resources/apiLogin.feature
@@ -1,16 +1,33 @@
 Feature: Login feature for api testing
 
-  Scenario: login user by api
+  Background: 
     Given correct url
+
+  Scenario: login user by api
     When user login with "emailId" and "password"
     Then veirfy status code 400
 
   Scenario: login user by api by correct user info
-    Given correct url
     When user login with "john.doe@gmail.com" and "123456"
     Then veirfy status code 200
 
-    #create Scenario outline, and pass below three sets of data
-    #invalid user name and password      ->400
-    #valid user name and invaid password ->400
-    #valid user name and valid password  ->200
+  #create Scenario outline, and pass below three sets of data
+  #invalid user name and password      ->404
+  #valid user name and invaid password ->400
+  #valid user name and valid password  ->200
+  Scenario Outline: login user by api by correct user info
+    When user login with "<email>" and "<password>"
+    Then veirfy status code <statusCode>
+
+    Examples: 
+      | email              | password | statusCode |
+      | invalid@gmail.com  |      234 |        404 |
+      | john.doe@gmail.com |      123 |        400 |
+      | john.doe@gmail.com |   123456 |        200 |
+      
+      
+@inClass
+  Scenario: register user by api call
+    When register user by "Nafisa" "nafisa4@gmail.com" "123456" "123456"
+    Then veirfy status code 200
+		And verify name "Nafisa" and email "nafisa@gmail.com" is correct
